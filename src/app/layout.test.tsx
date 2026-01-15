3329f0f7569769e57ace433e383b381e0105ed98
Waswa Olunga <olungaw@gmail.com>
2026-01-15 01:18:34 -0800

    feat: sets up app

---
+++ src/app/layout.test.tsx
@@ -0,0 +1,12 @@
+import { metadata } from "./layout";
+
+vi.mock("next/font/google", () => ({
+  ...vi.importActual("next/font/google"),
+  Roboto: () => ({
+    variable: "mock-font-variable",
+  }),
+}));
+
+it("should have the correct metadata title", () => {
+  expect(metadata.title).toBe("The Budget App");
+});
